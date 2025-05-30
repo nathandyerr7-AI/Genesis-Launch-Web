d3c6e7578005234c3ba9be83f91b74e0b7433155
nathandyerr7-AI <nathandyerr7@gmail.com>
2025-05-29 20:27:37 -0400

    Deleted vapi.tsx

--- src/components/ui/vapi.tsx
+++
@@ -1,6 +0,0 @@
-import Vapi from "@vapi-ai/web";
-
-const vapi = new Vapi();
-
-const call = await vapi.start();
-// { "id": "bd2184a1-bdea-4d4f-9503-b09ca8b185e6", "orgId": "6da6841c-0fca-4604-8941-3d5d65f43a17", "createdAt": "2024-11-13T19:20:24.606Z", "updatedAt": "2024-11-13T19:20:24.606Z", "type": "webCall", ... }
